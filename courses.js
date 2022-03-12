a0f1f3ef05f20a3323ebba54beaadefc9114cb87
Aditya Jain <61523512+AadityaJain-Dev@users.noreply.github.com>
2022-03-12 14:45:23 +0530

    adding new courses

--- courses.js
+++ courses.js
@@ -2,19 +2,19 @@
 
 module.exports = [
   // Beginner path - Vue 2
+  // "https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance",
   // "https://www.vuemastery.com/courses/intro-to-vue-js/list-rendering",
   // "https://www.vuemastery.com/courses/real-world-vue-js/real-world-intro",
   // "https://www.vuemastery.com/courses/mastering-vuex/intro-to-vuex",
   // "https://www.vuemastery.com/courses/next-level-vue/next-level-vue-orientation",
   // "https://www.vuemastery.com/courses/unit-testing/what-to-test",
-
   // Beginner path - Vue 3
+  // "https://www.vuemastery.com/courses/intro-to-vue-3/intro-to-vue3",
   // "https://www.vuemastery.com/courses/intro-to-vue-3/creating-the-vue-app-vue3",
   // "https://www.vuemastery.com/courses/real-world-vue3/rwv3-orientation",
   // "https://www.vuemastery.com/courses/touring-vue-router/vue-router-introduction",
   // "https://www.vuemastery.com/courses/vuex-fundamentals/vuex4-intro-to-vuex",
   // "https://www.vuemastery.com/courses/unit-testing-vue-3/what-to-test-vue-3/",
-
   // Intermediate path
   // "https://www.vuemastery.com/courses/real-world-testing/getting-started",
   // "https://www.vuemastery.com/courses/vue3-forms/forms-introduction",
@@ -26,7 +26,6 @@ module.exports = [
   // "https://www.vuemastery.com/courses/scaling-vue-with-nuxt-js/why-use-nuxt",
   // "https://www.vuemastery.com/courses/beautify-with-vuetify/getting-started-with-vuetify",
   // "https://www.vuemastery.com/courses/progressive-web-apps-vue-3/what-are-progressive-web-apps",
-
   // Advanced path
   // "https://www.vuemastery.com/courses/vue3-deep-dive-with-evan-you/vue3-overview",
   // "https://www.vuemastery.com/courses/vue3-typescript/why-vue-&-typescript",
@@ -38,4 +37,6 @@ module.exports = [
   // "https://www.vuemastery.com/courses/from-vue2-to-vue3/from-vue-2-to-vue-3",
   // "https://www.vuemastery.com/courses/advanced-components/the-introduction",
   // "https://www.vuemastery.com/courses/build-a-gmail-clone-with-vue3/tour-the-project",
+  // Vuex to Pinia
+  // "https://www.vuemastery.com/courses/from-vuex-to-pinia/what-is-pinia",
 ];
